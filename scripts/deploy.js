bcbdccb7248f44ebb395294ddb93903ee5000272
gwmccubbin <gwmccubbin@gmail.com>
2022-10-26 11:29:12 -0500

    Adds starter code

--- scripts/deploy.js
+++ scripts/deploy.js
@@ -6,57 +6,8 @@
 // global scope, and execute the script.
 const hre = require("hardhat");
 
-const tokens = (n) => {
-  return ethers.utils.parseUnits(n.toString(), 'ether')
-}
-
 async function main() {
-  // Setup accounts
-  const [buyer, seller, inspector, lender] = await ethers.getSigners()
 
-  // Deploy Real Estate
-  const RealEstate = await ethers.getContractFactory('RealEstate')
-  const realEstate = await RealEstate.deploy()
-  await realEstate.deployed()
-
-  console.log(`Deployed Real Estate Contract at: ${realEstate.address}`)
-  console.log(`Minting 3 properties...\n`)
-
-  for (let i = 0; i < 3; i++) {
-    const transaction = await realEstate.connect(seller).mint(`https://ipfs.io/ipfs/QmQVcpsjrA6cr1iJjZAodYwmPekYgbnXGo4DFubJiLc2EB/${i + 1}.json`)
-    await transaction.wait()
-  }
-
-  // Deploy Escrow
-  const Escrow = await ethers.getContractFactory('Escrow')
-  const escrow = await Escrow.deploy(
-    realEstate.address,
-    seller.address,
-    inspector.address,
-    lender.address
-  )
-  await escrow.deployed()
-
-  console.log(`Deployed Escrow Contract at: ${escrow.address}`)
-  console.log(`Listing 3 properties...\n`)
-
-  for (let i = 0; i < 3; i++) {
-    // Approve properties...
-    let transaction = await realEstate.connect(seller).approve(escrow.address, i + 1)
-    await transaction.wait()
-  }
-
-  // Listing properties...
-  transaction = await escrow.connect(seller).list(1, buyer.address, tokens(20), tokens(10))
-  await transaction.wait()
-
-  transaction = await escrow.connect(seller).list(2, buyer.address, tokens(15), tokens(5))
-  await transaction.wait()
-
-  transaction = await escrow.connect(seller).list(3, buyer.address, tokens(10), tokens(5))
-  await transaction.wait()
-
-  console.log(`Finished.`)
 }
 
 // We recommend this pattern to be able to use async/await everywhere
--- src/App.js
+++ src/App.js
@@ -14,81 +14,15 @@ import Escrow from './abis/Escrow.json'
 import config from './config.json';
 
 function App() {
-  const [provider, setProvider] = useState(null)
-  const [escrow, setEscrow] = useState(null)
-
-  const [account, setAccount] = useState(null)
-
-  const [homes, setHomes] = useState([])
-  const [home, setHome] = useState({})
-  const [toggle, setToggle] = useState(false);
-
-  const loadBlockchainData = async () => {
-    const provider = new ethers.providers.Web3Provider(window.ethereum)
-    setProvider(provider)
-    const network = await provider.getNetwork()
-
-    const realEstate = new ethers.Contract(config[network.chainId].realEstate.address, RealEstate, provider)
-    const totalSupply = await realEstate.totalSupply()
-    const homes = []
-
-    for (var i = 1; i <= totalSupply; i++) {
-      const uri = await realEstate.tokenURI(i)
-      const response = await fetch(uri)
-      const metadata = await response.json()
-      homes.push(metadata)
-    }
-
-    setHomes(homes)
-
-    const escrow = new ethers.Contract(config[network.chainId].escrow.address, Escrow, provider)
-    setEscrow(escrow)
-  }
-
-  useEffect(() => {
-    loadBlockchainData()
-  }, [])
-
-  const togglePop = (home) => {
-    setHome(home)
-    toggle ? setToggle(false) : setToggle(true);
-  }
 
   return (
     <div>
-      <Navigation account={account} setAccount={setAccount} />
-      <Search />
 
       <div className='cards__section'>
 
-        <h3>Homes For You</h3>
-
-        <hr />
-
-        <div className='cards'>
-          {homes.map((home, index) => (
-            <div className='card' key={index} onClick={() => togglePop(home)}>
-              <div className='card__image'>
-                <img src={home.image} alt="Home" />
-              </div>
-              <div className='card__info'>
-                <h4>{home.attributes[0].value} ETH</h4>
-                <p>
-                  <strong>{home.attributes[2].value}</strong> bds |
-                  <strong>{home.attributes[3].value}</strong> ba |
-                  <strong>{home.attributes[4].value}</strong> sqft
-                </p>
-                <p>{home.address}</p>
-              </div>
-            </div>
-          ))}
-        </div>
+        <h3>Welcome to Millow</h3>
 
       </div>
-
-      {toggle && (
-        <Home home={home} provider={provider} escrow={escrow} togglePop={togglePop} />
-      )}
 
     </div>
   );
